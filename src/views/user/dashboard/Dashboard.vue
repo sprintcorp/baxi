5cf1157de7f6319ed880fa41bfa257680885af7a
Adebola Fagbowore <bfagbowore@gmail.com>
2021-02-13 15:21:35 +0100

    update

--- src/views/user/dashboard/Dashboard.vue
+++ src/views/user/dashboard/Dashboard.vue
@@ -67,7 +67,7 @@
                             <div class="row p-3" v-if="cat" style="background-color:#dee2e645;margin-top:-20px">
                                 <div class="col-md-2" v-for="(category,index) in filerResult" :key="index">
                                     <!-- <router-link :to="{name:'categoryVendor',params: { id: category.id }}"> -->
-                                    <div class="card p-2" style="height:11rem;background-color:#ffc107;border:0px" @click="getCategoryProduct(category.id)">
+                                    <div class="card p-2" style="height:11rem;background-color:#ffc107;border:0px;width:12rem" @click="getCategoryProduct(category.id)">
                                         <!-- <div style="font-size:100px"><i class="fa fa-beer"></i></div> -->
                                         <div class="text-center mt-3"><img :src="category.public_image_url" class="rounded-circle" alt="" width="70" height="70"/></div>
                                         <div class="card-body text-center">
